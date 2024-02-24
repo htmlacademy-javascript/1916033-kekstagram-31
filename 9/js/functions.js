efc7cc5e92d9b25e401ec27ee79076c883d7dea4
Keks <keks@htmlacademy.ru>
2024-02-24 15:10:52 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/functions.js
@@ -0,0 +1,16 @@
+function getLength(string, maxLength) {
+  if (string.length <= maxLength) {
+    return true;
+  }
+  return false;
+}
+getLength('Длина строки', 10);
+function isPalindrome(string) {
+  const newString = (string.replaceAll(' ', '')).toLowerCase();
+  let emptyString = '';
+  for (let i = newString.length - 1; i >= 0; i--) {
+    emptyString += newString.at(i);
+  }
+  return emptyString === newString;
+}
+isPalindrome('топот');
